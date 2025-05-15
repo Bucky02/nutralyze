25b5695883a06f11a163a601f5a4ecb8d8128d49
Bucky02 <l.pauzano@studenti.unisa.it>
2025-05-15 16:22:48 +0200

    header con 3

--- src/components/Divdiary.jsx
+++ src/components/Divdiary.jsx
@@ -3,6 +3,13 @@ import './Divdiary.css'
 function Divdiary(){
     return(
         <div className='diaryBox'>
+            <h1>d</h1>
+            <h1>d</h1>
+            <h1>d</h1>
+            <h1>d</h1>
+            <h1>d</h1>
+            <h1>d</h1>
+            <h1>d</h1>
         </div>
 
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
-import './Header.css'
+import "./Header.css";
 
-function Header(){
-    return(
-        <div className='personalizzazione'>
-            <img src="/src/assets/logoIntero.png" alt="fwfwef" width='60px' height='60px'/>
-                        <p id='centrale'>NUTRALYZE</p>
-        </div>
-
-    )
+function Header() {
+  return (
+    <div className="personalizzazione">
+      <div id="img">
+        <img
+          src="/src/assets/logoIntero.png"
+          alt="fwfwef"
+          width="60px"
+          height="60px"
+        />
+      </div>
+      <div id="centrale">
+        <p>NUTRALYZE</p>
+      </div>
+      <div id="fine">
+        <p>FINE</p>
+      </div>
+    </div>
+  );
 }
-export default Header
+export default Header;
