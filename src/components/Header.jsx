c4affadbec9f6f6d27720136bf80ba15a7b5ce87
Bucky02 <l.pauzano@studenti.unisa.it>
2025-05-16 15:38:15 +0200

    menu tendina

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          src="/src/assets/logoIntero.png"
-          alt="Logo Nutralyze"
-          width="80px"
-          height="80px"
-        />
+        <Link to="/">
+          <img
+            src="/src/assets/logoIntero.png"
+            alt="Logo Nutralyze"
+            width="80px"
+            height="80px"
+          />
+        </Link>
       </div>
 
       <div className="title-container">
@@ -18,9 +24,24 @@ function Header() {
 
       <div className="buttons-container">
         <button className="login-button">Accedi</button>
-        <button className="secondary-button"></button>
+
+        <div className="dropdown">
+          <button
+            className="secondary-button"
+            onClick={() => setIsOpen((prev) => !prev)}
+          ></button>
+
+          {isOpen && (
+            <div className="dropdown-content">
+              <a href="#info1">Info 1</a>
+              <a href="#info2">Info 2</a>
+              <a href="#info3">Info 3</a>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 }
+
 export default Header;
